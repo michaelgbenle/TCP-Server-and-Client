c98a62aae67d2dca127c2a3ebf14967e1560a348
michaelgbenle <84715968+michaelgbenle@users.noreply.github.com>
2022-03-27 12:10:34 +0100

    ..

--- client/main.go
+++ client/main.go
@@ -10,35 +10,35 @@ import (
 	"strings"
 )
 
-func logFatal(err error){
+func logFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	connection, err:=net.Dial("tcp", ":8080")
+	connection, err := net.Dial("tcp", ":8080")
 	logFatal(err)
 
 	defer connection.Close()
 	fmt.Println("Enter your username:")
 
-	reader:=bufio.NewReader(os.Stdin)
-	username, err:=reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
+	username, err := reader.ReadString('\n')
 	logFatal(err)
-	username= strings.Trim(username," \r\n")
-	welcomeMessage:=fmt.Sprintf("Welcome %s\n", username)
-fmt.Println(welcomeMessage)
+	username = strings.Trim(username, " \r\n")
+	welcomeMessage := fmt.Sprintf("Welcome %s\n", username)
+	fmt.Println(welcomeMessage)
 
 	go read(connection)
-	write(connection,username)
+	write(connection, username)
 }
 
-func read(connection net.Conn){
-	for{
-		reader:=bufio.NewReader(connection)
-		message,err:=reader.ReadString('\n')
-		if err == io.EOF{
+func read(connection net.Conn) {
+	for {
+		reader := bufio.NewReader(connection)
+		message, err := reader.ReadString('\n')
+		if err == io.EOF {
 			connection.Close()
 			fmt.Println("connection closed")
 			os.Exit(0)
@@ -48,14 +48,14 @@ func read(connection net.Conn){
 	}
 }
 
-func write (connection net.Conn, username string){
+func write(connection net.Conn, username string) {
 	for {
-		reader:=bufio.NewReader(os.Stdin)
-		message,err:=reader.ReadString('\n')
-		if err != nil{
+		reader := bufio.NewReader(os.Stdin)
+		message, err := reader.ReadString('\n')
+		if err != nil {
 			break
 		}
-		message=fmt.Sprintf("%s:- %s\n", username, strings.Trim(message,"\r\n"))
+		message = fmt.Sprintf("%s:- %s\n", username, strings.Trim(message, "\r\n"))
 		connection.Write([]byte(message))
 	}
 }
--- server/main.go
+++ server/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"bufio"
-	
+
 	"log"
 	"net"
 )
 
-
-func logFatal(err error){
+func logFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,54 +15,48 @@ func logFatal(err error){
 
 var (
 	openConnect = make(map[net.Conn]bool)
- 	newConnect = make(chan net.Conn)
- 	deadConnect = make(chan net.Conn)
+	newConnect  = make(chan net.Conn)
+	deadConnect = make(chan net.Conn)
 )
 
-
-
-
 func main() {
-	ln, err :=net.Listen("tcp", ":8080")
-		logFatal(err)
+	ln, err := net.Listen("tcp", ":8080")
+	logFatal(err)
 
-		defer ln.Close()
+	defer ln.Close()
 
-go func ()  {
-	for {
-		conn, err := ln.Accept()
-		logFatal(err)
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			logFatal(err)
 
-		openConnect[conn] = true
-		newConnect <- conn
-	}
+			openConnect[conn] = true
+			newConnect <- conn
 
-}()
-// connection:= <-newConnect
-// reader:= bufio.NewReader(connection)
-// message, err :=reader.ReadString('\n')
-// logFatal(err)
-// fmt.Println(message)
-for  {
-	select{
-	case conn := <- newConnect:
-		go broadcastMessage(conn) 
-	case conn := <- deadConnect:
-		for item := range openConnect {
-			if item == conn {
-				break
-			}
 		}
-		delete(openConnect,conn)
-	}
-}
 
-}
+	}()
 
-func broadcastMessage(conn net.Conn)  {
 	for {
-		reader:= bufio.NewReader(conn)
-		message, err :=reader.ReadString('\n')
+		select {
+		case conn := <-newConnect:
+			broadcastMessage(conn)
+		case conn := <-deadConnect:
+			for item := range openConnect {
+				if item == conn {
+					break
+				}
+			}
+			delete(openConnect, conn)
+		}
+	}
+
+}
+
+func broadcastMessage(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(conn)
+		message, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
@@ -73,5 +66,5 @@ func broadcastMessage(conn net.Conn)  {
 			}
 		}
 	}
-	deadConnect<- conn
+	deadConnect <- conn
 }
